38191a30ffe8d60d284f6d00c49b55bff94bd844
HeyItsShuga <Shugabuga@users.noreply.github.com>
2017-11-20 11:56:03 -0500

    Version 1.0.1: Bug fixes

--- openskin.js
+++ openskin.js
@@ -46,59 +46,69 @@ function OpenSkin() {
   this.load = function() {
     var element = "";
 
-    element += ".navbar {" + sanLite(OpenSkin_jsonContents.styles[0].header) + ";" + "}";
-    element += "body {" + sanLite(OpenSkin_jsonContents.styles[0].label) + "}";
-    element += ".list-button, .button, .active {" + sanLite(OpenSkin_jsonContents.styles[0].button) + "}";
-    element += ".modal {" + sanLite(OpenSkin_jsonContents.styles[0].alert) + "}";
-    element += ".page-content {" + sanLite(OpenSkin_jsonContents.styles[0].containerBackground) + "}";
-    element += ".item-content, .card, .swiper-slide {" + sanLite(OpenSkin_jsonContents.styles[0].cell) + "}";
-    element += ".item-after {" + sanLite(OpenSkin_jsonContents.styles[0].cellChevron) + "}";
-    element += ".toolbar {" + sanLite(OpenSkin_jsonContents.styles[0].toolbar) + "}";
+    element += ".navbar {" + sanLite(jsonContents.styles[0].header) + ";" + "}";
+    element += "body {" + sanLite(jsonContents.styles[0].label) + "}";
+    element += ".list-button, .button, .active {" + sanLite(jsonContents.styles[0].button) + "}";
+    element += ".modal {" + sanLite(jsonContents.styles[0].alert) + "}";
+    element += ".page-content {" + sanLite(jsonContents.styles[0].containerBackground) + "}";
+    element += ".item-content, .card, .swiper-slide {" + sanLite(jsonContents.styles[0].cell) + "}";
+    element += ".item-after {" + sanLite(jsonContents.styles[0].cellChevron) + "}";
+    element += ".toolbar {" + sanLite(jsonContents.styles[0].toolbar) + "}";
     // iGBA methods are officially in the OpenSkin spec.
-    element += "#emulatorPopup {" + sanLite(OpenSkin_jsonContents.styles[0].emulatorPopup) + ";" + "}";
-    element += "#triggerL {" + sanLite(OpenSkin_jsonContents.styles[0].triggerL) + "}";
-    element += "#triggerR {" + sanLite(OpenSkin_jsonContents.styles[0].triggerR) + "}";
-    element += "#center {" + sanLite(OpenSkin_jsonContents.styles[0].center) + "}";
-    element += "#right {" + sanLite(OpenSkin_jsonContents.styles[0].right) + "}";
-    element += "#left {" + sanLite(OpenSkin_jsonContents.styles[0].left) + "}";
-    element += "#up {" + sanLite(OpenSkin_jsonContents.styles[0].up) + "}";
-    element += "#down {" + sanLite(OpenSkin_jsonContents.styles[0].down) + "}";
-    element += "#aBtn {" + sanLite(OpenSkin_jsonContents.styles[0].aBtn) + "}";
-    element += "#bBtn {" + sanLite(OpenSkin_jsonContents.styles[0].bBtn) + "}";
-    element += "#startbtn {" + sanLite(OpenSkin_jsonContents.styles[0].startBtn) + "}";
-    element += "#select {" + sanLite(OpenSkin_jsonContents.styles[0].select) + "}";
+    element += "#emulatorPopup {" + sanLite(jsonContents.styles[0].emulatorBackground) + ";" + "}";
+    element += "#emulatorTarget {" + sanLite(jsonContents.styles[0].emulatorScreen) + ";" + "}";
+    element += "#triggerL {" + sanLite(jsonContents.styles[0].triggerL) + "}";
+    element += "#triggerR {" + sanLite(jsonContents.styles[0].triggerR) + "}";
+    element += "#center {" + sanLite(jsonContents.styles[0].center) + "}";
+    element += "#right {" + sanLite(jsonContents.styles[0].right) + "}";
+    element += "#left {" + sanLite(jsonContents.styles[0].left) + "}";
+    element += "#up {" + sanLite(jsonContents.styles[0].up) + "}";
+    element += "#down {" + sanLite(jsonContents.styles[0].down) + "}";
+    element += "#aBtn {" + sanLite(jsonContents.styles[0].aBtn) + "}";
+    element += "#bBtn {" + sanLite(jsonContents.styles[0].bBtn) + "}";
+    element += "#startbtn {" + sanLite(jsonContents.styles[0].startBtn) + "}";
+    element += "#select {" + sanLite(jsonContents.styles[0].select) + "}";
 
-    if(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName]) {
-      element += ".navbar {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].header) + ";" + "}";
-      element += "body {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].label) + "}";
-      element += ".list-button, .button, .active {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].button) + "}";
-      element += ".modal {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].alert) + "}";
-      element += ".page-content {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].containerBackground) + "}";
-      element += ".item-content, .card, .swiper-slide {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].cell) + "}";
-      element += ".item-after {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].cellChevron) + "}";
-      element += ".toolbar {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].toolbar) + "}";
+    if(jsonContents.styles[0][jsonAppName]) {
+      element += ".navbar {" + sanLite(jsonContents.styles[0][jsonAppName][0].header) + ";" + "}";
+      element += "body {" + sanLite(jsonContents.styles[0][jsonAppName][0].label) + "}";
+      element += ".list-button, .button, .active {" + sanLite(jsonContents.styles[0][jsonAppName][0].button) + "}";
+      element += ".modal {" + sanLite(jsonContents.styles[0][jsonAppName][0].alert) + "}";
+      element += ".page-content {" + sanLite(jsonContents.styles[0][jsonAppName][0].containerBackground) + "}";
+      element += ".item-content, .card, .swiper-slide {" + sanLite(jsonContents.styles[0][jsonAppName][0].cell) + "}";
+      element += ".item-after {" + sanLite(jsonContents.styles[0][jsonAppName][0].cellChevron) + "}";
+      element += ".toolbar {" + sanLite(jsonContents.styles[0][jsonAppName][0].toolbar) + "}";
       // iGBA methods are officially in the OpenSkin spec.
-      element += "#emulatorPopup {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].emulatorPopup) + ";" + "}";
-      element += "#triggerL {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].triggerL) + "}";
-      element += "#triggerR {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].triggerR) + "}";
-      element += "#center {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].center) + "}";
-      element += "#right {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].right) + "}";
-      element += "#left {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].left) + "}";
-      element += "#up {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].up) + "}";
-      element += "#down {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].down) + "}";
-      element += "#aBtn {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].aBtn) + "}";
-      element += "#bBtn {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].bBtn) + "}";
-      element += "#startbtn {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].startBtn) + "}";
-      element += "#select {" + sanLite(OpenSkin_jsonContents.styles[0][OpenSkin_jsonAppName][0].select) + "}";
+      element += "#emulatorPopup {" + sanLite(jsonContents.styles[0][jsonAppName][0].emulatorBackground) + ";" + "}";
+      element += "#emulator_target {" + sanLite(jsonContents.styles[0][jsonAppName][0].emulatorScreen) + ";" + "}";
+      element += "#triggerL {" + sanLite(jsonContents.styles[0][jsonAppName][0].triggerL) + "}";
+      element += "#triggerR {" + sanLite(jsonContents.styles[0][jsonAppName][0].triggerR) + "}";
+      element += "#center {" + sanLite(jsonContents.styles[0][jsonAppName][0].center) + "}";
+      element += "#right {" + sanLite(jsonContents.styles[0][jsonAppName][0].right) + "}";
+      element += "#left {" + sanLite(jsonContents.styles[0][jsonAppName][0].left) + "}";
+      element += "#up {" + sanLite(jsonContents.styles[0][jsonAppName][0].up) + "}";
+      element += "#down {" + sanLite(jsonContents.styles[0][jsonAppName][0].down) + "}";
+      element += "#aBtn {" + sanLite(jsonContents.styles[0][jsonAppName][0].aBtn) + "}";
+      element += "#bBtn {" + sanLite(jsonContents.styles[0][jsonAppName][0].bBtn) + "}";
+      element += "#startbtn {" + sanLite(jsonContents.styles[0][jsonAppName][0].startBtn) + "}";
+      element += "#select {" + sanLite(jsonContents.styles[0][jsonAppName][0].select) + "}";
     }
 
     // Custom CSS
-    element += OpenSkin_jsonContents.styles[0].custom[0][OpenSkin_jsonAppName]
+    try {
+      element += jsonContents.styles[0].custom[0][jsonAppName]
+    } catch(n) {
+      //
+    }
 
-    var style = document.createElement("style");
-    style.id = "openskin_stylesheet";
-    style.innerHTML = element;
-    document.head.appendChild(style);
+    if(document.getElementById("openskin_stylesheet")) {
+      document.getElementById("openskin_stylesheet").innerHTML = element;
+    } else {
+      var style = document.createElement("style");
+      style.id = "openskin_stylesheet";
+      style.innerHTML = element;
+      document.head.appendChild(style);
+    }
 
     return true
   }
